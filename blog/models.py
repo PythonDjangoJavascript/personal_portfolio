e31f8c1d5c3d87ec47068c9b23a2772baecf2988
Sayed <nuruddinsayeed@gmail.com>
2020-06-02 23:29:23 +0600

    Blog Database added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+
+    """This Class is to create blog databse"""
+
+    title = models.CharField(max_length = 100)
+    description = models.TextField()
+    date = models.DateField()
