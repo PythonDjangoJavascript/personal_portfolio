cba14c4bc75c3f8a574bae963b1aebb06e2c6413
Sayed <nuruddinsayeed@gmail.com>
2020-06-04 00:22:16 +0600

    Blog detail page added

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Blog
 def all_blogs(request):
 
 
-    blogs = Blog.objects.order_by('-date')[:5]   # to limit last 5 blogs and order by date
+    blogs = Blog.objects.order_by('-date') #[:5]   # to limit last 5 blogs and order by date
     return render(request, 'blog/all_blogs.html', {'blogs' : blogs})
 
 def detail(request, blog_id):
